ec55246937be4d7fdc0664d5609ec9583012b6a0
AnamMansur111 <anammansur000@gmail.com>
2026-08-31 17:44:26 +0530

    Max

---
+++ Max.java
@@ -0,0 +1,22 @@
+public class Max {
+    public static void main(String[] args) {
+
+        int[] arr = { 1, 3, 5, 6, 7, 8, 9, 10 };
+        System.out.println(max(arr));
+    }
+
+    static int max(int[] arr) {
+        int maxVal = arr[0];
+        for (int i = 0; i < arr.length;i++){
+            if(arr[i]>maxVal){
+                maxVal=arr[i];
+
+            }
+           
+        }
+        return maxVal;
+    }
+
+}
+
+   
