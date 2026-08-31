053860d84972e5cc539a4ae236c793f3ff75a250
AnamMansur111 <anammansur000@gmail.com>
2026-08-31 18:54:36 +0530

    reverse

--- Swap.java
+++ Swap.java
@@ -1,10 +1,23 @@
 import java.util.Arrays;
 public class Swap {
     public static void main(String[] args) {
-        int[] arr = { 2, 4, 5, 8, 1, };
-        swap(arr , 1, 3);
+        int[] arr = { 2, 4, 5, 8, 1 };
+        reverse(arr);
         System.out.println(Arrays.toString(arr));
-     }
+    }
+
+    static void reverse(int[]arr){
+         int start=0;    
+         int end=arr.length-1;
+        while(start <end ) {
+            swap(arr,start,end);
+            start++;
+            end--;
+        }
+    }
+        
+    
+  
       static void swap(int[]arr,int index1,int index2){
             int temp=arr[index1];
             arr[index1]=arr[index2];
