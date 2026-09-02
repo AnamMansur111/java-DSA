fbeb33502483536bad1f00b9f92dae5ea8d5fccf
AnamMansur111 <anammansur000@gmail.com>
2026-09-02 22:54:23 +0530

    Floor cd /Users/anam/Desktop/Java-DSA ; /usr/bin/env /Library/Java/JavaVirtualMachines/jdk-24.jdk/Contents/Home/bin/java -XX:+ShowCodeDetailsInExceptionMessages -cp /Users/anam/Library/Application\ Support/Code/User/workspaceStorage/dccfff26def5c1c20d13d5f4fc0c9abb/redhat.java/jdt_ws/Java-DSA_c106b3fa/bin Floor

---
+++ Floor.java
@@ -0,0 +1,24 @@
+public class Floor {
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 5, 7, 8, 9, 10 };
+        int target = 6;
+        int ans = Floor(arr, target);
+        System.out.println(ans);
+    }
+
+    static int Floor(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return end;
+    }
+}
